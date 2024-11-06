b78817253db5f09b592bd918db687a9a065e1844
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-11-06 13:12:10 +0530

    signup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import { SignupFormDemo } from "@/components/auth/SignupForm";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div>
-        <h1>hello kutuukare</h1>
-      </div>
+     
+      <SignupFormDemo/>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,44 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
+// Custom plugin to add CSS variables for colors
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
 
-
+  addBase({
+    ":root": newVars,
+  });
+}
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx}", // Additional content path as per your requirement
   ],
+  darkMode: "class", // Enable dark mode
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        ...colors, // Add Tailwind's default colors as a base
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans], // Extending the default font
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors], // Register the custom plugin
 };
+
 export default config;
