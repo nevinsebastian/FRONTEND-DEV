6a0b8757cabc8859c8a63211efdcb9f7628192ff
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-11-06 21:28:50 +0530

    signup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { SignupFormDemo } from "@/components/auth/SignupForm";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-     
-      <SignupFormDemo/>
+      {/* BackgroundBeams with a lower z-index */}
+      <div className="absolute inset-0 -z-10">
+        <BackgroundBeams />
+      </div>
+      
+      {/* SignupFormDemo with a higher z-index */}
+      
+        <SignupFormDemo />
+      
     </main>
   );
 }
--- components/auth/SignupForm.tsx
+++ components/auth/SignupForm.tsx
@@ -34,20 +34,27 @@ export function SignupFormDemo() {
             <Input id="lastname" placeholder="Durden" type="text" />
           </LabelInputContainer>
         </div>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
           <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
+          <Label htmlFor="otp">OTP</Label>
+          <Input id="otp" placeholder="••••••" type="otp" />
+        </LabelInputContainer>
+        </div>
+        <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
           <Input id="password" placeholder="••••••••" type="password" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-8">
-          <Label htmlFor="twitterpassword">Your twitter password</Label>
+          <Label htmlFor="confirmpassword">Confirm password</Label>
           <Input
-            id="twitterpassword"
+            id="confirmpassword"
             placeholder="••••••••"
-            type="twitterpassword"
+            type="confirmpassword"
           />
         </LabelInputContainer>
 
@@ -82,16 +89,7 @@ export function SignupFormDemo() {
             </span>
             <BottomGradient />
           </button>
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
-            <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              OnlyFans
-            </span>
-            <BottomGradient />
-          </button>
+          
         </div>
       </form>
     </div>
