1264c0e5e45280af0728130cb63eedfc60e03eb8
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-11-08 16:35:35 +0530

    structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { SignupFormDemo } from "@/components/auth/SignupForm";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import { Cover } from "@/components/ui/cover";
 
+
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center overflow-hidden mx-auto sm:px-10 px-5 min-h-screen">
@@ -13,14 +15,19 @@ export default function Home() {
       {/* Main content with two columns */}
       <div className="w-full flex flex-col sm:flex-row justify-between items-center max-w-6xl">
         {/* Left Section for Text */}
-        <div className="w-full sm:w-1/2 text-white sm:pr-10 mb-10 sm:mb-0">
-          <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-            Build amazing websites <br /> at <Cover>warp speed</Cover>
+        <div className="w-full sm:w-1/2 text-white sm:pr-10 mb-10 sm:mb-0 flex flex-col justify-center">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-8 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+            Transform Your Onboarding Process at <Cover>warp speed</Cover>
           </h1>
-          <h1 className="text-3xl font-bold mb-4">Welcome to Our Platform</h1>
-          <p className="text-lg">
-            Experience seamless onboarding, manage customer relationships, and
-            simplify your workflow with our all-in-one platform.
+          <h2 className="text-2xl lg:text-3xl font-bold mt-4 mb-6 text-center sm:text-left">
+            Welcome to Our Platform
+          </h2>
+          <p className="text-lg md:text-xl leading-relaxed text-center sm:text-left max-w-2xl mx-auto sm:mx-0">
+            Experience efficiency like never before. Accelerate customer
+            onboarding, streamline employee verification, and manage your entire
+            workflow seamlessly. With roles-based dashboards and robust CRM
+            integrations, our all-in-one solution empowers your team to work
+            smarter, not harder
           </p>
         </div>
 
