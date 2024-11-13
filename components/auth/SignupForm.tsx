6e7e31fd3a3684f86829c9b88a3b71c3a2872c4e
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-11-13 23:38:18 +0530

    signup rounded

--- components/auth/SignupForm.tsx
+++ components/auth/SignupForm.tsx
@@ -21,7 +21,7 @@ export function SignupFormDemo() {
   };
 
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black h-280 mt-10">
+    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome to Gluping
       </h2>
