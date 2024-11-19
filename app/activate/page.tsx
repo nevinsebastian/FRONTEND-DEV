39ab79c81fb4c7e9f81df9ddd46175c41b61d52c
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-11-19 17:53:22 +0530

    activation

--- app/activate/page.tsx
+++ app/activate/page.tsx
@@ -12,7 +12,7 @@ const ActivatePage = () => {
       <BackgroundGradientAnimation />
 
       {/* Centered Form */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-md p-4">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-dvh max-h-lg p-4">
         <ActivationForm />
       </div>
     </div>
