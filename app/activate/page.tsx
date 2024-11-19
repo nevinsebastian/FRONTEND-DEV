5aa4046e4585658173c806741ccbe285fe7b4261
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-11-19 17:08:37 +0530

    activation

--- app/activate/page.tsx
+++ app/activate/page.tsx
@@ -5,7 +5,7 @@ import { ActivationForm } from "@/components/form/activationform";
 const ActivatePage = () => {
   return (
     <div className="activation-page">
-      <h1 className="text-2xl font-bold mb-4">Activate Your Account</h1>
+      <div></div>
       <ActivationForm />
     </div>
   );
