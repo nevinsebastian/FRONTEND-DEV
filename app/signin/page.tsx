1bd994aac711ce433f923c8f1ae421ad72e89223
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-11-21 13:10:33 +0530

    sales dashboard

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,12 @@
+// SignInPage.js
 import { SignIn } from "./signinform";
 
 export default function SignInPage() {
-  return <SignIn />;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-md px-4 py-6 shadow-md rounded-md sm:mx-auto sm:w-[90%]">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
