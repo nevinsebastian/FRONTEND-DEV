89a9be887ce38dea37652469faa1cd2f7d368895
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-11-23 15:44:26 +0530

    bug fix

--- app/activate/page.tsx
+++ app/activate/page.tsx
@@ -2,14 +2,11 @@
 
 import React from "react";
 import { ActivationForm } from "@/components/form/activationform";
-import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
-import { cn } from "@/lib/utils";
 
 const ActivatePage = () => {
   return (
     <div className="relative min-h-screen w-full">
       {/* Background Gradient Animation */}
-      <BackgroundGradientAnimation />
 
       {/* Centered Form */}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-dvh max-h-lg p-4">
