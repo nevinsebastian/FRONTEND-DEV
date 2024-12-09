d02d61ddb55ebcc0504997bf6220c19ba9fe5a42
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-12-09 16:36:45 +0530

    login fixed

--- app/signin/signin.ts
+++ app/signin/signin.ts
@@ -1,11 +1,11 @@
 export const signIn = async (email: string, password: string) => {
   const formData = new URLSearchParams();
-  formData.append("grant_type", "");
+  formData.append("grant_type", "x");
   formData.append("username", email);
   formData.append("password", password);
-  formData.append("scope", "");
-  formData.append("client_id", "");
-  formData.append("client_secret", "");
+  formData.append("scope", "x");
+  formData.append("client_id", "x");
+  formData.append("client_secret", "x");
 
   const response = await fetch("https://3.111.52.81:8000/login", {
     method: "POST",
